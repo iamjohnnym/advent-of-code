e81f4264f5ee515b076f89cdd5b19bcebe39d80d
John Martin <johnny@iamjohnnym.com>
2018-12-06 19:47:53 -0800

    moved files for `part1` into its own dir so that i can start on `part2` in a similar manner.

--- go/day1/part1/main.go
+++ go/day1/part1/main.go
@@ -11,7 +11,7 @@ import (
 var numbers []int
 
 func main() {
-	content := lib.LoadFile("challenge.txt")
+	content := lib.LoadFile("../challenge.txt")
 	for _, line := range strings.Split(content, "\n") {
 		// convert `line`string to `n`int and append []int{numbers}
 		if n, err := strconv.Atoi(line); err == nil {
