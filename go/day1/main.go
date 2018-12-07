41c325a26d0f8ebad9758adf9f7de2de567a7c8c
John Martin <johnny@iamjohnnym.com>
2018-12-06 19:32:46 -0800

    * Cleanup of packages based on changes to project config

--- go/day1/main.go
+++ go/day1/main.go
@@ -2,28 +2,21 @@ package main
 
 import (
 	"fmt"
-	"lib"
 	"strconv"
 	"strings"
+
+	"github.com/iamjohnnym/advent-of-code/go/lib"
 )
 
 var numbers []float64
 
-func sum(frequencies []float64) float64 {
-	total := 0.0
-	for _, value := range frequencies {
-		total += value
-	}
-	return total
-}
-
 func main() {
-	content := lib.ReadChallengeFile("challenge.txt")
+	content := lib.LoadFile("challenge.txt")
 	for _, line := range strings.Split(string(content), "\n") {
 		if n, err := strconv.ParseFloat(line, 64); err == nil {
 			numbers = append(numbers, n)
 		}
 	}
-	total := sum(numbers)
+	total := lib.Sum(numbers)
 	fmt.Println(total)
 }
--- go/lib/packages.go
+++ go/lib/packages.go
@@ -7,6 +7,7 @@ import (
 
 // file name of the input file
 var fileName string
+var iterator []float64
 
 // LoadFile Load input file as a string
 func LoadFile(fileName string) string {
@@ -16,4 +17,12 @@ func LoadFile(fileName string) string {
 		return "Unable to read file"
 	}
 	return string(content)
+}
+
+func Sum(iterator []float64) float64 {
+	total := 0.0
+	for _, value := range iterator {
+		total += value
+	}
+	return total
 }
